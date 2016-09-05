7c84c423f99607e46bde0ba18b9b29cb6d06c0d9
Alfie <alfie_ab@hotmail.co.uk>
2016-09-05 17:59:21 +0100

    link model added and properly tested

--- app/app.rb
+++ app/app.rb
@@ -3,8 +3,14 @@ require 'data_mapper'
 require 'dm-postgres-adapter'
 
 class BookMark < Sinatra::Base
+
   get '/' do
-    'Hello book-mark!'
+    'Hello'
+  end
+
+  get '/links' do
+    @links = Link.all
+    erb :'links/index'
   end
 
   # start the server if ruby file executed directly
