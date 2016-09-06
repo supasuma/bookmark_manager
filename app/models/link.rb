c48a6485600c2da36a1ae5e36c7ba57655a3f2f0
supasuma <supasuma@gmail.com>
2016-09-06 13:21:47 +0100

    datamapper.setup

--- app/models/link.rb
+++ app/models/link.rb
@@ -10,7 +10,7 @@ class Link
 
 end
 
-DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
 
 DataMapper.finalize
 
