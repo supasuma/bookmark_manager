47c9cc87141b4c5cc47e842a899a3b88646e8c84
supasuma <supasuma@gmail.com>
2016-09-07 18:10:17 +0100

    completed 19

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,21 @@ require 'bcrypt'
 
 class User
   include DataMapper::Resource
-  #include BCrypt
 
   property :id,     Serial
   property :email,  String
   property :password_digest, Text
 
+  attr_reader :password
+  attr_accessor :password_confirmation
+
+  validates_confirmation_of :password
+
   def password=(password)
+    @password = password
     self.password_digest = BCrypt::Password.create(password)
-  end  
+  end
+
+
 
 end
--- spec/feature/user_sign_up_spec.rb
+++ spec/feature/user_sign_up_spec.rb
@@ -1,7 +1,11 @@
-feature 'Siging up' do
+feature 'Signing up' do
   scenario "when user signs up user count increases by one" do
     expect{ sign_up }.to change(User, :count).by 1
     expect(page).to have_content('Welcome, summerisawesome@gmail.com')
     expect(User.first.email).to eq('summerisawesome@gmail.com')
+  end
+
+  scenario 'user sign up requires a matching password' do
+    expect{ sign_up(password_confirmation: 'wrong') }.not_to change(User, :count)
   end
 end
