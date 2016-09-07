8a05fdeb73300280d7f5e1633ec2f9ff33a9dbed
johnnydee8 <j.dworzynski@me.com>
2016-09-07 17:06:17 +0100

    poo

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -3,6 +3,9 @@ require 'dm-postgres-adapter'
 
 require_relative 'models/tag'
 require_relative 'models/link'
+require_relative 'models/user'
+
+ENV['RACK_ENV']||= "development"
 
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
 
