062ca956adc7513631aea67d7faeb074fe234857
Alfie <alfie_ab@hotmail.co.uk>
2016-09-08 15:27:08 +0100

    prevented against invalid email addresses

--- spec/features/users_mgmt_spec.rb
+++ spec/features/users_mgmt_spec.rb
@@ -4,12 +4,25 @@ feature 'User sign up' do
   scenario 'I can sign up as a new user' do
     expect{ sign_up }.to change(User, :count).by 1
     expect(page).to have_content('Welcome, testuser1')
-    expect(User.first.email).to eq('testuser1')
+    expect(User.first.email).to eq('testuser1@gmail.com')
   end
 
   scenario 'will not allow user to log in' do
     expect{ wrong_sign_up }.to change(User, :count).by 0
     expect(current_path).to eq '/users'
     expect(page).to have_content("You've entered your password wrong")
+  end
+
+  scenario 'will not allow user to log in without an email address' do
+    visit 'users/new'
+    fill_in :email, with: nil
+    click_button 'Sign up'
+    expect(current_path).to eq '/users'
+  end
+
+  scenario 'will not allow user to log in with an invalid email address' do
+    visit 'users/new'
+    fill_in :email, with: 'alfie@com' 
+    expect{click_button 'Sign up'}.to change(User, :count).by 0
   end
 end
--- spec/web_helper.rb
+++ spec/web_helper.rb
@@ -1,7 +1,7 @@
 def sign_up
   visit '/users/new'
   expect(page.status_code).to eq(200)
-  fill_in :email, with: 'testuser1'
+  fill_in :email, with: 'testuser1@gmail.com'
   fill_in :password, with: 'my_secret_password'
   fill_in :password_confirmation, with: 'my_secret_password'
   click_button 'Sign up'
@@ -10,7 +10,7 @@ end
 def wrong_sign_up
   visit '/users/new'
   expect(page.status_code).to eq(200)
-  fill_in :email, with: 'testuser1'
+  fill_in :email, with: 'testuser1@gmail.com'
   fill_in :password, with: 'my_secret_password'
   fill_in :password_confirmation, with: 'wrong_password'
   click_button 'Sign up'
