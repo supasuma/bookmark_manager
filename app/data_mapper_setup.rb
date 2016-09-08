065ffb990c1a6fe8a2119439ad18b623b08066ff
Alfie <alfie_ab@hotmail.co.uk>
2016-09-08 11:17:29 +0100

    last change

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -7,7 +7,6 @@ require_relative 'models/user'
 
 ENV['RACK_ENV']||= "development"
 
-ENV['RACK_ENV'] ||= "development"
 
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
 
